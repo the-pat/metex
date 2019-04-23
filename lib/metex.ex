5b2a669e9c7aea228d95681155310eda4ee7c290
Patrick Tone <pt@patrickt.one>
2019-04-23 18:25:36 -0500

    Use `Task` to make multiple concurrent API calls

--- lib/metex.ex
+++ lib/metex.ex
@@ -1,13 +1,44 @@
 defmodule Metex do
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {Task.Supervisor, name: Metex.RequestSupervisor}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+
   def temperatures_of(cities) do
-    Metex.Worker.start_link()
+    client =
+      Application.get_env(:metex, :weather_api_key)
+      |> Metex.Weather.client()
 
     cities
-    |> Task.async_stream(&Metex.Worker.get_temperature/1)
+    |> Enum.map(
+      &Task.Supervisor.async_nolink(Metex.RequestSupervisor, fn ->
+        Metex.Weather.get_weather(client, &1)
+        |> parse_response()
+      end)
+    )
+    |> Task.yield_many(5000)
     |> Enum.map(fn {task, res} ->
-      res || Task.shutdown(task, :brutal_kill)
+      {:ok, weather} = res || Task.shutdown(task, :brutal_kill)
+
+      weather
     end)
     |> Enum.sort()
-    |> Enum.to_list()
+  end
+
+  defp parse_response({:ok, %Tesla.Env{status: 200, body: body}}) do
+    temp =
+      (body["main"]["temp"] - 273.15)
+      |> Float.round(1)
+
+    {:ok, body["name"], temp}
+  end
+
+  defp parse_response(_) do
+    :error
   end
 end
